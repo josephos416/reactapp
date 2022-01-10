50fd0281ecc310b08d6a4ff27798fb409afcba4e
Joseph Perez <jpere219@calpoly.edu>
2022-01-09 17:11:16 -0800

    final

--- src/MyApp.js
+++ src/MyApp.js
@@ -2,39 +2,39 @@ import React, {useState} from 'react';
 import Table from './Table';
 
 
-function MyApp() {
-  const [characters, setCharacters] = useState([
-      {
-        name: 'Charlie',
-        job: 'Janitor',
-         // the rest of the data
-      },
-      {
-        name: 'Mac',
-        job: 'Bouncer',
-      },
-      {
-        name: 'Dee',
-        job: 'Aspring actress',
-      },
-      {
-        name: 'Dennis',
-        job: 'Bartender',
-      },
-    ]);
+import Form from './Form';
 
+
+
+function MyApp() {
+  const [characters, setCharacters] = useState([]);
+  function updateList(person) {
+    setCharacters([...characters, person]);
+  }
 function removeOneCharacter (index) {
   const updated = characters.filter((character, i) => {
       return i !== index
     });
     setCharacters(updated);
   }
-  return (
-    <div className="container">
-      <Table characterData={characters} removeCharacter={removeOneCharacter} />
-    </div>
-  )
+
+
+return (
+  <div className="container">
+    <Table characterData={characters} removeCharacter={removeOneCharacter} />
+    <Form handleSubmit={updateList} />
+
+
+  </div>
+)
+
+
 }
+
+
+
+
+
 
 
 
